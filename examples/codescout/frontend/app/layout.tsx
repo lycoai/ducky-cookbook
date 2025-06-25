8d311ef94ae7da60e5e566b548d52594740873e2
leonel <leonell_ceolin@hotmail.com>
2025-06-25 10:24:44 -0300

    add helvetica font family

--- examples/codescout/frontend/app/layout.tsx
+++ examples/codescout/frontend/app/layout.tsx
@@ -1,16 +1,5 @@
 import type { Metadata } from 'next'
-import { Geist, Geist_Mono } from 'next/font/google'
 import './globals.css'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin']
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin']
-})
 
 export const metadata: Metadata = {
   title: 'CodeScout',
@@ -26,8 +15,8 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable} `}>
-      <body className={`font-sans antialiased`}>{children}</body>
+    <html lang="en">
+      <body className="antialiased">{children}</body>
     </html>
   )
 }
